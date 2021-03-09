e93999000b9726820ab40fdd4b40bb17ae1264e9
ArudoPE <aortizparodi@outlook.com>
2021-03-09 11:23:24 -0500

    First commit, basic repository structure

---
+++ index.js
@@ -0,0 +1,8 @@
+function hideCurrencyTable() {
+    document.getElementById("currencyTable").style.display="none";
+    document.getElementById("comisionTable").style.display="block";
+}
+function hideComisionTable() {
+    document.getElementById("comisionTable").style.display="none";
+    document.getElementById("currencyTable").style.display="block";
+}
