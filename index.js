60af4689290ada0beb2a5d63688b099a9f7558fd
ArudoPE <aortizparodi@outlook.com>
2021-03-09 13:35:29 -0500

    Added basic js for card slides

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Funciones de tablas
 function hideCurrencyTable() {
     document.getElementById("currencyTable").style.display="none";
     document.getElementById("comisionTable").style.display="block";
@@ -6,3 +7,34 @@ function hideComisionTable() {
     document.getElementById("comisionTable").style.display="none";
     document.getElementById("currencyTable").style.display="block";
 }
+//Funciones de Planes
+
+//Desde recommendedCard
+function showBasicCard() {
+    document.getElementById("basicPlanCard").style.display="block";
+    document.getElementById("recommendedPlanCard").style.display="none";
+}
+function showUnlimitedCard() {
+    document.getElementById("unlimitedPlanCard").style.display="block";
+    document.getElementById("recommendedPlanCard").style.display="none";
+}
+//Desde unlimitedCard
+function showRecommendedCard() {
+    document.getElementById("recommendedPlanCard").style.display="block";
+    document.getElementById("unlimitedPlanCard").style.display="none";
+}
+function showBasicCard2() {
+    document.getElementById("basicPlanCard").style.display="block";
+    document.getElementById("unlimitedPlanCard").style.display="none";
+}
+//Desde basicCard
+function showRecommendedCard2() {
+    document.getElementById("recommendedPlanCard").style.display="block";
+    document.getElementById("basicPlanCard").style.display="none";
+}
+function showUnlimitedCard2() {
+    document.getElementById("unlimitedPlanCard").style.display="block";
+    document.getElementById("basicPlanCard").style.display="none";
+}
+
+
